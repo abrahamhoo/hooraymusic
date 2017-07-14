e5883919e90e988618eba8c2d988d678eb9afec5
rhu <rhu@i9i8.com>
2017-07-14 11:39:39 +0800

    2 commit

--- resources/views/partials/grid.blade.php
+++ resources/views/partials/grid.blade.php
@@ -1,14 +1,34 @@
 <div class="row grid">
-    <div class="col-md-3 text-center content">
-        <img class="img-thumbnail" style="width: 180px; height: 180px" src="{{asset('assets/img/1989.jpg')}}" >
+    <div class="col-md-3 text-center content item">
+        @if(isset($data[$key]))
+            <img class="img-thumbnail cover" style="width: 180px; height: 180px" src="{{asset($data[$key]['cover'])}}" >
+            <span class="name">{{$data[$key]['name']}}</span>
+            <hr class="line"/>
+            <span class="artist">{{$data[$key]['artist']}}</span>
+        @endif
     </div>
-    <div class="col-md-3 text-center content">
-        <img class="img-thumbnail" style="width: 180px; height: 180px" src="{{asset('assets/img/1989.jpg')}}" >
+    <div class="col-md-3 text-center content item">
+        @if(isset($data[$key+1]))
+            <img class="img-thumbnail cover" style="width: 180px; height: 180px" src="{{asset($data[$key+1]['cover'])}}" >
+            <span class="name">{{$data[$key+1]['name']}}</span>
+            <hr class="line"/>
+            <span class="artist">{{$data[$key+1]['artist']}}</span>
+        @endif
     </div>
-    <div class="col-md-3 text-center content">
-        <img class="img-thumbnail" style="width: 180px; height: 180px" src="{{asset('assets/img/1989.jpg')}}" >
+    <div class="col-md-3 text-center content item">
+        @if(isset($data[$key+2]))
+            <img class="img-thumbnail cover" style="width: 180px; height: 180px" src="{{asset($data[$key+2]['cover'])}}" >
+            <span class="name">{{$data[$key+2]['name']}}</span>
+            <hr class="line"/>
+            <span class="artist">{{$data[$key+2]['artist']}}</span>
+        @endif
     </div>
-    <div class="col-md-3 text-center content">
-        <img class="img-thumbnail" style="width: 180px; height: 180px" src="{{asset('assets/img/1989.jpg')}}" >
+    <div class="col-md-3 text-center content item">
+        @if(isset($data[$key+3]))
+            <img class="img-thumbnail cover" style="width: 180px; height: 180px" src="{{asset($data[$key+3]['cover'])}}" >
+            <span class="name">{{$data[$key+3]['name']}}</span>
+            <hr class="line"/>
+            <span class="artist">{{$data[$key+3]['artist']}}</span>
+        @endif
     </div>
 </div>
